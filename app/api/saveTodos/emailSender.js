042e7406155e5836d9afda624c1fc9db3b98ca71
Arnva <75409785+Arnva@users.noreply.github.com>
2023-06-27 22:02:06 -0400

    Update emailSender.js
    added a placeholder email and password before AWS email Functionality to test if it works on vercel

--- app/api/saveTodos/emailSender.js
+++ app/api/saveTodos/emailSender.js
@@ -2,29 +2,27 @@
 import { createTransport } from "nodemailer";
 
 async function emailSender(email,todos) {
-  console.log(`heelo ${email} , ${todos}`)
-  // await delay(1000);
-  // console.log("working till emailsender");
+  console.log("working till emailsender");
 
-  // let transporter = createTransport({
-  //   host: 'smtp-mail.outlook.com',
-  //   port: 587,
-  //   secure: false,
-  //   auth: {
-  //       user: '',
-  //       pass: '',}
-  // });
+  let transporter = createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'ReminderBeforeAWS@outlook.com',
+        pass: 'TooLazyToUseEnvVariablesSinceThisIsAPlaceholder',}
+  });
 
-  // // send mail with defined transport object
-  // let info = await transporter.sendMail({
-  //   from: '"arnav Shah" <arnavshah116@outlook.com>',
-  //   to: decodeURIComponent(email), 
-  //   subject: 'Reminders Today',
-  //   text: todos,
-  //   html: `<b> ${todos} </b>`,
-  // });
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Reminder Email" <ReminderBeforeAWS@outlook.com>',
+    to: decodeURIComponent(email), 
+    subject: 'Reminders Today',
+    text: todos,
+    html: `<b> ${todos} </b>`,
+  });
 
-  // console.log("Message sent: %s", info.messageId);
+  console.log("Message sent: %s", info.messageId);
 }
 
 export default emailSender;
